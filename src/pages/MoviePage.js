d50bdb0911db670e43374ad1e686d0d17d7b1790
AlvinTuan <letuan7923@gmail.com>
2023-06-01 10:19:54 +0700

    +pag

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MovieList from "../components/movie/MovieList";
 import useSWR from "swr";
 import { fetcher } from "../config";
 import MovieCard from "../components/movie/MovieCard";
+import useDebounce from "../hooks/useDebounce";
+import ReactPaginate from "react-paginate";
 
+const itemsPerPage = 20;
 const MoviePage = () => {
-  const { data } = useSWR(
-    `https://api.themoviedb.org/3/movie/popular?api_key=0f2828258a4259490e4802ec8bceef93`,
-    fetcher
+  const [nextPage, setNextPage] = useState(1);
+  const [filter, setFilter] = useState("");
+  const [url, setUrl] = useState(
+    `https://api.themoviedb.org/3/movie/popular?api_key=0f2828258a4259490e4802ec8bceef93&page=${nextPage}`
   );
+  const filterDebounce = useDebounce(filter, 500);
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+  const { data, error } = useSWR(url, fetcher);
+  const loading = !data && !error;
+  useEffect(() => {
+    if (filterDebounce) {
+      setUrl(
+        `https://api.themoviedb.org/3/search/movie?api_key=0f2828258a4259490e4802ec8bceef93&query=${filterDebounce}&page=${nextPage}`
+      );
+    } else {
+      setUrl(
+        `https://api.themoviedb.org/3/movie/popular?api_key=0f2828258a4259490e4802ec8bceef93&page=${nextPage}`
+      );
+    }
+  }, [filterDebounce, nextPage]);
   const movies = data?.results || [];
+  const [itemOffset, setItemOffset] = useState(0);
+  if (!data || !data.total_results) return;
+  const pageCount = Math.ceil(data.total_results / itemsPerPage);
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % data.total_results;
+    setItemOffset(newOffset);
+    setNextPage(event.selected + 1);
+  };
   return (
     <div className="py-10 page-container">
       <div className="flex mb-10">
@@ -18,6 +47,7 @@ const MoviePage = () => {
             type="text"
             className="w-full p-4 bg-slate-800 text-white outline-none"
             placeholder="Type here to search..."
+            onChange={handleFilterChange}
           />
         </div>
         <button className="p-4 bg-primary text-white">
@@ -37,11 +67,76 @@ const MoviePage = () => {
           </svg>
         </button>
       </div>
+      {/* tạo nút xoay */}
+      {loading && (
+        <div className="w-10 h-10 rounded-full border-4 border-primary border-t-transparent border-t-4 mx-auto animate-spin"></div>
+      )}
       <div className="grid grid-cols-4 gap-10 ">
-        {movies.length > 0 &&
+        {!loading &&
+          movies.length > 0 &&
           movies.map((item) => (
             <MovieCard key={item.id} item={item}></MovieCard>
           ))}
+      </div>
+      {/* // them phan trang */}
+      {/* <div className="flex items-center justify-center mt-10 gap-x-5 hidden">
+        <span className="cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+            onClick={() => setNextPage(nextPage - 1)}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 19.5L8.25 12l7.5-7.5"
+            />
+          </svg>
+        </span>
+        {new Array(numberPage).fill(0).map((item, index) => (
+          <span
+            key={index}
+            className="cursor-pointer inline-block py-2 px-4 rounded leading-none bg-white text-slate-900"
+            onClick={() => setNextPage(index + 1)}
+          >
+            {index + 1}
+          </span>
+        ))}
+
+        <span className="cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+            onClick={() => setNextPage(nextPage + 1)}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </span>
+      </div> */}
+      {/* phan trang su dung thu vien */}
+      <div className="mt-10">
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel=">"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="<"
+          renderOnZeroPageCount={null}
+          className="pagination"
+        />
       </div>
     </div>
   );
