8969eae442624c2435985b4f0c5b45390961ec8a
AlvinTuan <letuan7923@gmail.com>
2023-06-04 14:03:50 +0700

    load-bounding

--- src/components/movie/MovieCard.js
+++ src/components/movie/MovieCard.js
@@ -2,6 +2,9 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../button/Button";
 import { tmdbAPI } from "../../config";
+import PropTypes from "prop-types";
+import { withErrorBoundary } from "react-error-boundary";
+import LoadingSkeleton from "../loading/LoadingSkeleton";
 
 const MovieCard = ({ item }) => {
   const { title, release_date, vote_average, poster_path, id } = item;
@@ -33,4 +36,70 @@ const MovieCard = ({ item }) => {
   );
 };
 
-export default MovieCard;
+MovieCard.propTypes = {
+  item: PropTypes.shape({
+    title: PropTypes.string,
+    release_date: PropTypes.string,
+    vote_average: PropTypes.number,
+    poster_path: PropTypes.string,
+    id: PropTypes.number,
+  }),
+};
+
+function FallbackComponent() {
+  return (
+    <p className="bg-red-50 text-red-400">
+      Something went wrong with this components
+    </p>
+  );
+}
+
+export const MovieCardSkeleton = () => {
+  return (
+    <div className="movie-card flex flex-col rounded-lg p-3 bg-slate-800 text-white h-full select-none">
+      {/* <img
+      src={tmdbAPI.imageW500(poster_path)}
+      alt=""
+      className="w-full h-[250px] object-cover rounded-lg mb-5"
+    /> */}
+      <LoadingSkeleton
+        width="100%"
+        height="250px"
+        radius="8px"
+        className="mb-5"
+      ></LoadingSkeleton>
+      <div className="flex flex-col flex-1">
+        <h3 className="text-xl font-bold mb-3">
+          <LoadingSkeleton width="100%" height="20px"></LoadingSkeleton>
+        </h3>
+        <div className="flex item-center justify-between text-sm opacity-50 mb-10">
+          {/* <span>{new Date(release_date).getFullYear()}</span> */}
+          <span>
+            <LoadingSkeleton width="50px" height="10px"></LoadingSkeleton>
+          </span>
+          <span>
+            <LoadingSkeleton width="30px" height="10px"></LoadingSkeleton>
+          </span>
+        </div>
+        <LoadingSkeleton
+          width="100%"
+          height="45px"
+          radius="6px"
+        ></LoadingSkeleton>
+        {/* <Button onClick={() => navigate(`/movie/${id}`)} bgColor="secondary">
+        Watch now
+      </Button> */}
+        {/* <button
+          onClick={() => navigate(`/movie/${id}`)}
+          className="py-3 px-6 rounded-lg capitalize bg-primary w-full mt-auto"
+        >
+          Watch now
+        </button> */}
+      </div>
+    </div>
+  );
+};
+
+export default withErrorBoundary(MovieCard, {
+  FallbackComponent,
+});
